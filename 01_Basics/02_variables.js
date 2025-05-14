f7735a7ea812cb7f81dfd97b2d72f6ca2448aba2
HIREN3827 <dadhaniyahiren2580@gmail.com>
2025-05-14 20:22:44 +0000

    change night

---
+++ 01_Basics/02_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "hirendadhaniya2580@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ahmedabad"
+accountState 
+
+// accountId = 2 // not allowed
+
+accountEmail ="himanshu@gmail.com"
+accountPassword = "54321"
+accountCity = "haydrabad"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
