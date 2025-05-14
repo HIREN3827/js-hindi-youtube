708b868ffa14f386c34cfe9d5e4299504fbbb508
HIREN3827 <dadhaniyahiren2580@gmail.com>
2025-05-14 19:44:30 +0000

    dataTypes

---
+++ 01_Basics/01_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs , not browser
+
+console.log(3+3);
+
+console.log("Hiren");
+
+let name = "Hiren"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => "" or ''
+// boolen => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
