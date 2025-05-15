a4f0a78685fa2945753f38f29a79c2f152c91ea3
HIREN3827 <dadhaniyahiren2580@gmail.com>
2025-05-15 16:59:07 +0000

    conversions of data types

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,2 +1,35 @@
-let score = 33 
+let score = "Hiren"
 
+// console.log(typeof score);
+// console.log(typeof (score)); 
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);  
+// console.log(valueInNumber);  
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+// null => 0
+// undefined => undefined
+
+let isLoggedIn = "Hiren"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false;
+// "" => false ; "value(hiren)" => true
+
+
+// This method is use to convert any variable value into any another value like number into sring , string into numers , and many more 
+
+// Example is here 
+
+let aNumber = 32
+
+let conString = String(aNumber)
+
+console.log(conString);
+console.log(typeof conString);
